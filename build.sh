022253ddb28c456a3aaf53df70bb160a0a6716e1
Hydrog3n <loic.vaille@gmail.com>
2018-02-05 13:24:30 +0100

    Fix version buildroot

--- build.sh
+++ build.sh
@@ -2,22 +2,34 @@
 
 PROJECTPATH=`pwd`
 BUILDROOTDIR="../buildroot"
-if [ ! -d $BUILDROOTDIR ]; then 
+if [ ! -d $BUILDROOTDIR ]; then
+	echo "add buildroot" 
 	git clone git://git.buildroot.net/buildroot ../buildroot
+	cd $BUILDROOTDIR
+	git checkout 2017.11.x
+	git pull origin 2017.11.x
 	make beaglebone_defconfig
 	make
+else
+	echo "Update buildroot"
+	cd $BUILDROOTDIR
+	git checkout 2017.11.x
+	git pull origin 2017.11.x
 fi
 
-echo $BUILDROOTDIR
+cd $PROJECTPATH
 
 ## Copy all packages in build root
+echo "Copy packages"
 cp -r package/* $BUILDROOTDIR/package
 
 ## Prepare buildroot config and target
+echo "Copy config"
 cp ./config/.config $BUILDROOTDIR
 cp -r ./target/* $BUILDROOTDIR/output/target/
 
 ## Build 
+echo "Build"
 cd $BUILDROOTDIR
 make
 
